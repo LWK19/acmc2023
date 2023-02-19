df50c125ef8148a0c0b2832905b01f512b41744c
Lee Wai Kin <leewaikin19@gmail.com>
2023-02-19 22:56:20 +0800

    asd

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var qnlink;
 function str_pad_left(string, pad, length) {
     return (new Array(length + 1).join(pad) + string).slice(-length);
 }
-//asdsadad
+//should be 2023
 
 
 function instructTimer() {
